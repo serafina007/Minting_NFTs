16f5b9dec297fa13c185a60e1a624ead4c808fe0
serafina007 <zeyuqi@seas.upenn.edu>
2025-11-03 15:43:18 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -11,7 +11,7 @@ def sign_challenge( challenge ):
     prove that you have claimed your NFT.
     
     This is the only line you need to modify in this file before you submit """
-    sk = "YOUR SECRET KEY HERE"
+    sk = "0xbd66528a"
 
     acct = w3.eth.account.from_key(sk)
 
